a787b185f96a96b1b7f9e159e55def36a366314e
Monserrat <monserratdiaz12@gmail.com>
2016-12-17 21:37:36 +0000

    add javascript

---
+++ time.js
@@ -0,0 +1,17 @@
+/* global $ */
+var current=0;
+
+function nextQuestion() {
+   var next = current + 1;
+    var currentSel = '.question' + current;
+   var nextSel ='.question' + next;
+   
+   $(currentSel).hide();
+   $(nextSel).show();
+   
+   current+= 1;
+}
+
+$('.selection').click(function() {
+    nextQuestion();
+});
